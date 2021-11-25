2183a974d1257fa6b5a900fcb0cf56d9fb7f5243
Trust Okerezi <trustokerezi42@gmail.com>
2021-11-25 21:33:58 +0100

    Slider UI created

---
+++ src/data.js
@@ -0,0 +1,29 @@
+import headphone_black from './images/headphone_black.png'
+import headphone_green from './images/headphone_green.png'
+import headphone_pink from './images/headphone_pink.png'
+
+export const sliderItems = [
+    {
+        id: 1,
+        img: headphone_black,
+        title: "Black Friday Sale.",
+        desc: "GET FLAT 30% OFF NEW ARRIVALS.",
+        bg: "f5fafd"
+    },
+
+    {
+        id: 2,
+        img: headphone_green,
+        title: "Black Friday Sale.",
+        desc: "GET FLAT 30% OFF NEW ARRIVALS.",
+        bg: "d8e2dc"
+    },
+
+    {
+        id: 3,
+        img: headphone_pink,
+        title: "Black Friday Sale.",
+        desc: "GET FLAT 30% OFF NEW ARRIVALS.",
+        bg: "fae1dd"
+    },
+]
