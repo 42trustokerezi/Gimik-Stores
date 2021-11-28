92275a0fd8fe693fb5e47a170ff96e7448879276
Trust Okerezi <trustokerezi42@gmail.com>
2021-11-28 23:53:37 +0100

    Popular Product UI created

--- src/data.js
+++ src/data.js
@@ -1,10 +1,22 @@
+//SLIDER
 import headphone_black from './images/headphone_black.png'
 import headphone_green from './images/headphone_green.png'
 import headphone_pink from './images/headphone_pink.png'
 
+//CATEGORIES
 import phones from './images/iphone_13.jpeg';
 import ipads from './images/ipad_pro.jpeg';
 import macbook from './images/macbook_pro.jpeg';
+
+//POPULAR_PRODUCTS 
+import apple_ipad_pro from './images/apple_ipad_pro.jpeg';
+import apple_pad_mini from './images/apple_pad-mini.jpeg';
+import google_pixel3 from './images/google_pixel3.jpeg';
+import nokia_dual_sim from './images/nokia_2.4_dual_sim.jpeg';
+import samsung_galaxy_tab from './images/samsung_galaxy_tab.jpeg';
+import tecno_camon_17_pro from './images/tecno_camon_17_pro.jpeg';
+import samsung_galaxy_ultra from './images/samsung_galaxy_ultra.jpeg';
+
 
 export const sliderItems = [
     {
@@ -49,4 +61,35 @@ export const categories =[
         img: macbook,
         title: 'LAPTOP'
     }
+]
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: samsung_galaxy_ultra
+    },
+    {
+        id: 2,
+        img: apple_ipad_pro
+    },
+    {
+        id: 3,
+        img: apple_pad_mini
+    },
+    {
+        id: 4,
+        img: google_pixel3
+    },
+    {
+        id: 5,
+        img: nokia_dual_sim
+    },
+    {
+        id: 6,
+        img: tecno_camon_17_pro
+    },
+    {
+        id: 7,
+        img: samsung_galaxy_tab
+    },
 ]
