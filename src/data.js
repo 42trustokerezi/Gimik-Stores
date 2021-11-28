8da190e5a21d63026ffee7311837f534e1d7a04e
Trust Okerezi <trustokerezi42@gmail.com>
2021-11-28 21:26:44 +0100

    Product-Categories UI created

--- src/data.js
+++ src/data.js
@@ -2,6 +2,10 @@ import headphone_black from './images/headphone_black.png'
 import headphone_green from './images/headphone_green.png'
 import headphone_pink from './images/headphone_pink.png'
 
+import phones from './images/iphone_13.jpeg';
+import ipads from './images/ipad_pro.jpeg';
+import macbook from './images/macbook_pro.jpeg';
+
 export const sliderItems = [
     {
         id: 1,
@@ -26,4 +30,23 @@ export const sliderItems = [
         desc: "GET FLAT 30% OFF NEW ARRIVALS.",
         bg: "fae1dd"
     },
+]
+
+
+export const categories =[
+    {
+        id: 1,
+        img: phones,
+        title: 'PHONES'
+    },
+    {
+        id: 2,
+        img: ipads,
+        title: 'IPAD'
+    },
+    {
+        id: 3,
+        img: macbook,
+        title: 'LAPTOP'
+    }
 ]
