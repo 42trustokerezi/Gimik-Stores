41717694228e9ea91accf1b5199098e0ad30effa
Trust Okerezi <trustokerezi42@gmail.com>
2021-12-01 13:18:11 +0100

    shopping category created

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -2,7 +2,9 @@
 import React from 'react'
 import styled from 'styled-components'
 import Announcement from '../components/Announcement'
+import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
+import Newsletter from '../components/Newsletter'
 import Products from '../components/Products'
 
 const Container = styled.div`
@@ -19,6 +21,18 @@ const Filter = styled.div`
     margin: 20px;
 `
 
+const FilterText = styled.span`
+    font-size: 20px;
+    font-weight: 600;
+    margin-right: 20px;
+`
+
+const Select = styled.select`
+    padding: 10px;
+    margin-right: 20px;
+`
+const Option = styled.option``
+
 const ProductList = () => {
     return (
         <Container>
@@ -26,10 +40,40 @@ const ProductList = () => {
             <Navbar/>
             <Title>Phones</Title>
             <FilterContainer>
-                <Filter>filter1</Filter>
-                <Filter>filter1</Filter>
+                <Filter><FilterText>Filter Products:</FilterText>
+                    <Select>
+                        <Option disabled selected>
+                            Color
+                        </Option>
+                        <Option>White</Option>
+                        <Option>Black</Option>
+                        <Option>Red</Option>
+                        <Option>Blue</Option>
+                        <Option>Yellow</Option>
+                        <Option>Green</Option>
+                    </Select>
+                    <Select>
+                        <Option disabled selected>
+                            Brand
+                        </Option>
+                        <Option>Apple</Option>
+                        <Option>Samsung</Option>
+                        <Option>Xiaomi</Option>
+                        <Option>Nokia</Option>
+                        <Option>Infinix</Option>
+                    </Select>
+                </Filter>
+                <Filter><FilterText>Sort Products:</FilterText>
+                <Select>
+                    <Option selected>Newest</Option>
+                    <Option>Price (asc)</Option>
+                    <Option>Price (des)</Option>
+                </Select>
+                </Filter>
             </FilterContainer>
             <Products/>
+            <Newsletter/>
+            <Footer/>
         </Container>
     )
 }
