3df5fe3c526ee01d9d55ec8456b86d7517dc6480
Trust Okerezi <trustokerezi42@gmail.com>
2021-12-01 15:05:45 +0100

    registration and login UI created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 //import Home from "./pages/Home";
 
-import Product from "./pages/Product";
+import Login from "./pages/Login";
+
+//import Register from "./pages/Register";
+
+//import Product from "./pages/Product";
 
 //import ProductList from "./pages/ProductList";
 
 function App() {
   return (
-    <Product/>
+    <Login/>
   );
 }
 
