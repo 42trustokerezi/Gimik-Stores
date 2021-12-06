50e22ea70b62f4c343321c5a607a7391016144b1
Trust Okerezi <trustokerezi42@gmail.com>
2021-12-06 09:52:49 +0100

    responsive ui

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -6,10 +6,12 @@ import Footer from '../components/Footer';
 
 import nokia_dual from '../images/nokia_2.4_dual_sim.jpeg';
 import { Add, Remove } from '@material-ui/icons';
+import { mobile } from '../responsive';
 
 const Container = styled.div``;
 const Wrapper = styled.div`
     padding: 20px;
+    ${mobile({padding: "10px"})}
 `;
 const Title = styled.h1`
     font-weight: 300;
@@ -30,7 +32,9 @@ const TopButton = styled.button`
     background-color:${props=>props.type === "filled" ? "black" : "transparent"};
     color: ${props=>props.type === "filled" && "white"};
 `
-const TopTexts = styled.div``
+const TopTexts = styled.div`
+    ${mobile({display: "none"})}
+`
 const TopText = styled.span`
     text-decoration: underline;
     cursor: pointer;
@@ -40,6 +44,7 @@ const TopText = styled.span`
 const Bottom = styled.div`
     display: flex;
     justify-content: space-between;
+    ${mobile({flexDirection: "column"})}
 `;
 const Info = styled.div`
     flex: 3;
@@ -48,6 +53,7 @@ const Info = styled.div`
 const Product = styled.div`
     display: flex;
     justify-content: space-between;
+    ${mobile({flexDirection: "column"})}
 `
 const Hr = styled.hr`
     background-color: #eee;
@@ -92,10 +98,12 @@ const ProductAmountContainer = styled.div`
 const ProductAmount = styled.div`
     font-size: 24px;
     margin: 5px;
+    ${mobile({margin: "5px 15px"})}
 `
 const ProductPrice = styled.div`
-    font-size: 30px;
-    font-weight:200
+    font-size: 24px;
+    font-weight:400;
+    ${mobile({marginBottom: "20px"})}
 `
 
 const Summary = styled.div`
@@ -106,11 +114,27 @@ const Summary = styled.div`
     height: 50vh;
 `;
 
-const SummaryTitle  = styled.div``
-const SummaryItem  = styled.div``
+const SummaryTitle  = styled.div`
+    font-weight: 200;
+`
+const SummaryItem  = styled.div`
+    margin: 30px 0px;
+    display: flex;
+    justify-content: space-between;
+    font-weight: ${props=>props.type === "total" && "500"};
+    font-size: ${props=>props.type === "total" && "24px"};
+`
 const SummaryItemText  = styled.span``
 const SummaryItemPrice  = styled.span``
-const SummaryButton  = styled.button``
+const SummaryButton  = styled.button`
+    width: 100%;
+    padding: 10px;
+    background-color: black;
+    border: none;
+    color: white;
+    font-weight: 600;
+    cursor: pointer;
+`
 
 const Cart = () => {
     return (
@@ -118,7 +142,7 @@ const Cart = () => {
             <Navbar />
             <Announcement/>
             <Wrapper>
-                <Title>YOUR BAG</Title>
+                <Title>YOUR CART</Title>
                 <Top>
 
                     <TopButton>CONTINUE SHOPPING</TopButton>
@@ -175,7 +199,7 @@ const Cart = () => {
                     <SummaryTitle>ORDER SUMMARY</SummaryTitle>
                     <SummaryItem>
                         <SummaryItemText>Subtotal</SummaryItemText>
-                        <SummaryItemText>160,000.00 NGN</SummaryItemText>
+                        <SummaryItemPrice>160,000.00 NGN</SummaryItemPrice>
                     </SummaryItem>
                     <SummaryItem>
                         <SummaryItemText>Estimated Delivery</SummaryItemText>
@@ -183,11 +207,11 @@ const Cart = () => {
                     </SummaryItem>
                     <SummaryItem>
                         <SummaryItemText>Delivery Discount</SummaryItemText>
-                        <SummaryItemText>-200.00 NGN</SummaryItemText>
+                        <SummaryItemPrice>-200.00 NGN</SummaryItemPrice>
                     </SummaryItem>
-                    <SummaryItem>
-                        <SummaryItemText type="total">Total</SummaryItemText>
-                        <SummaryItemText>160,000.00 NGN</SummaryItemText>
+                    <SummaryItem type="total">
+                        <SummaryItemText>Total</SummaryItemText>
+                        <SummaryItemPrice>160,000.00 NGN</SummaryItemPrice>
                     </SummaryItem>
                     <SummaryButton>CHECKOUT NOW</SummaryButton>
                     </Summary>
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -6,11 +6,14 @@ import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import Newsletter from '../components/Newsletter'
 import iphone_13 from '../images/iphone_13.jpeg'
+import { mobile } from '../responsive'
 
 const Container = styled.div``
+
 const Wrapper = styled.div`
     padding: 50px;
     display: flex;
+    ${mobile({ padding: '10px',flexDirection: "column"})}
 `
 const ImgContainer = styled.div`
     flex: 1;
@@ -18,11 +21,13 @@ const ImgContainer = styled.div`
 const InfoContainer = styled.div`
     flex: 1;
     padding: 0px 20px;
+    ${mobile({padding: "10px"})}
 `
 const Image = styled.img`
     width: 80%;
     height: 70vh;
     object-fit: fit;
+    ${mobile({height: "40vh"})}
 `
 const Title = styled.h1`
     font-weight: 200;
@@ -40,6 +45,7 @@ const FilterContainer = styled.div`
     margin: 30px 0px;
     display: flex;
     justify-content: space-between;
+    ${mobile({width: "100%"})}
 `
 const Filter = styled.div`
     display: flex;
@@ -68,6 +74,7 @@ const AddContainer = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
+    ${mobile({width: "100%"})}
 `
 const AmountContainer = styled.div`
     display: flex;
